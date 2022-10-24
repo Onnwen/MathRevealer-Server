19cf1805b9c02218b264f425467874763b659b41
Onnwen <onnwen.cassitto@icloud.com>
2022-10-24 20:37:36 +0200

    Register endpoint

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,9 @@ const {json} = require("express");
 /* GET | /users/id */
 router.get('/:id', async function(req, res, next) {
     try {
-        let userInformations = await users.getUserById(req.params.id);
-        if (userInformations.length) {
-            res.json(userInformations[0]);
+        let userInformation = await users.getUserById(req.params.id);
+        if (userInformation.length) {
+            res.json(userInformation[0]);
         }
         else {
             res.json({message: "User does not exist."});
@@ -19,16 +19,27 @@ router.get('/:id', async function(req, res, next) {
     }
 });
 
-/* POST | /users/login */
+/* POST - /users/login */
 router.post('/login', async function(req, res, next) {
     try {
-        let userInformations = await users.getUserByCredentials(req.body);
-        if (userInformations.length) {
-            res.json({status_code: 1, userInformations: userInformations[0]});
+        let userInformation = await users.getUserByCredentials(req.body);
+        if (userInformation.length) {
+            res.json({status_code: 1, userInformation: userInformation[0]});
         }
         else {
             res.json({status_code: 0});
         }
+    } catch (err) {
+        console.error(`Error`, err.message);
+        next(err);
+    }
+});
+
+/* POST - /users/register */
+router.post('/register', async function(req, res, next) {
+    try {
+        let responseCode = await users.addUser(req.body);
+        res.json({status_code: responseCode});
     } catch (err) {
         console.error(`Error`, err.message);
         next(err);
--- services/users.js
+++ services/users.js
@@ -3,22 +3,36 @@ const helper = require('../helper');
 const config = require('../config');
 
 async function getUserById(id) {
-    const userInformations = await db.query(`SELECT Users.id, Users.first_name, Users.last_name
+    const userInformation = await db.query(`SELECT Users.id, Users.first_name, Users.last_name
                                              FROM Users
                                              WHERE Users.id = ${id}`);
-    return helper.emptyOrRows(userInformations);
+    return helper.emptyOrRows(userInformation);
 }
 
 async function getUserByCredentials(credentials) {
     console.log(credentials);
-    const userInformations = await db.query(`SELECT Users.id, Users.first_name, Users.last_name, Users.email
+    const userInformation = await db.query(`SELECT Users.id, Users.first_name, Users.last_name, Users.email
                                              FROM Users
                                              WHERE Users.email = '${credentials.email}'
                                                AND Users.hashed_password = '${credentials.hashed_password}'`);
-    return helper.emptyOrRows(userInformations);
+    return helper.emptyOrRows(userInformation);
+}
+
+async function addUser(userInformation) {
+    if (userInformation.first_name === undefined || userInformation.last_name === undefined || userInformation.hashed_password === undefined || userInformation.email === undefined) {
+        return 0;
+    }
+    try {
+        await db.query(`INSERT INTO Users (first_name, last_name, hashed_password, email) VALUES ('${userInformation.first_name}', '${userInformation.last_name}', '${userInformation.hashed_password}', '${userInformation.email}')`);
+        return 1;
+    }
+    catch (e) {
+        return -1;
+    }
 }
 
 module.exports = {
     getUserById,
-    getUserByCredentials
+    getUserByCredentials,
+    addUser
 }
