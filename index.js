bcddd1ad50ca0a78c66bb6b0d219233400e9e755
Onnwen <onnwen.cassitto@icloud.com>
2022-10-24 20:37:17 +0200

    Cambiamenti minori

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ app.use("/users", users);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     console.error(err.message, err.stack);
-    res.status(statusCode).json({message: err.message});
+    res.status(statusCode).json({message_error: err.message});
     return;
 });
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
