936fb26b48ebcc08461481cb7c6d4b6176bccf81
Onnwen <onnwen.cassitto@icloud.com>
2022-11-12 20:15:20 +0100

    Aggiornata configurazione database

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     db: {
-        host: "192.168.64.2",
-        user: "mathRevealerServer",
-        password: "S8eAub^%F1ABx^BY#82yeJOXPty$IB9o^#uta&7F#e8H4$Uf3y",
+        host: "localhost",
+        user: "root",
+        password: "",
         database: "mathRevealer",
     },
     listPerPage: 10,
