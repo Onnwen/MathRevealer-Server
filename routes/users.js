087933488e8cfcb51c4aaaf732977e4005066838
Onnwen <onnwen.cassitto@icloud.com>
2022-12-28 21:59:17 +0100

    Miglioramento sicurezza

--- routes/users.js
+++ routes/users.js
@@ -95,7 +95,7 @@ router.post('/register', async function(req, res, next) {
 /* POST - /users/saveExpression*/
 router.post('/saveExpression', async function(req, res, next) {
     try {
-        let responseCode = await users.saveExpression(req.body.expression, req.body.user_id);
+        let responseCode = await users.saveExpression(req.body.expression, req.session.userInformation.id);
         res.json(responseCode);
     } catch (err) {
         console.error(`Error`, err.message);
