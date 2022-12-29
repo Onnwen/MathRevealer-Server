ddeb6906bd25e3c5c8e229bb1cfa102c460cca3c
Onnwen <onnwen.cassitto@icloud.com>
2022-12-29 11:47:08 +0100

    Risolto bug

--- services/users.js
+++ services/users.js
@@ -1,9 +1,4 @@
 const db = require('./db');
-const bcrypt = require("bcrypt");
-const Uuid = require("uuid");
-const mysql = require("mysql2/promise");
-const config = require("../config");
-const {sendVerificationEmail} = require("./mailer");
 
 async function getUserById(id) {
     return await db.query(`SELECT users.id, users.first_name, users.last_name
@@ -13,6 +8,5 @@ async function getUserById(id) {
 
 module.exports = {
     getUserById,
-    login,
     register
 }
