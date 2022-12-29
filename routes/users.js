bf58cb3b2f010dbd7fb98a6013752d09d4dbbd82
Onnwen <onnwen.cassitto@icloud.com>
2022-12-29 11:50:29 +0100

    Risolto bug

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const users = require('../services/users');
 
 /* GET | /users/id */
-router.get('/get/:id', async function(req, res, next) {
+router.get('/:id', async function(req, res, next) {
     try {
         let userInformation = await users.getUserById(req.params.id);
         if (userInformation.length) {
